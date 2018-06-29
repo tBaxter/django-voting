cf2cbfc142d41693f9291d411491e04d637deb51
TBaxter <mail.baxter@gmail.com>
2018-06-29 09:17:11 -0500

    Updates for Django 2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='tango-voting',
-    version='0.3.2',
+    version='0.4.0',
     author='Jonathan Buchanan/Tim Baxter',
     author_email='mail.baxter@gmail.com',
     url='https://github.com/tBaxter/django-voting',
